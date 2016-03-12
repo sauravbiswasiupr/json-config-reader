ec8c999e894f938ea2306875324f5c4ce4d22cb9
Saurav Biswas <saurav@sauravs-mbp.speedport_w723_v_typ_a_1_01_012>
2016-03-13 00:42:17 +0100

    Added some files

---
+++ config_test.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAssertConfigVarIsNumber(t *testing.T) {
+	filename := "./static/config.json"
+	var bytesRead []byte
+	bytesRead = Read(filename)
+	configMap := ReadJSON(bytesRead)
+
+	memcacheTimeout := configMap["MEMCACHE_TIMEOUT"]
+	isNumber, err := IsFloat(memcacheTimeout)
+
+	if isNumber != true {
+		t.Error(err)
+	}
+}
+
+func TestAssertWrongType(t *testing.T) {
+	configurationMap := CreateConfigMap([]string{"./static/config.json"})
+	dbUrl := configurationMap["DATABASE_URL"]
+	stringVal, err := IsFloat(dbUrl)
+	assert.NotNil(t, err)
+	assert.Equal(t, stringVal, false)
+}
